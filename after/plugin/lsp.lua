b0666ee800a42879f6445b1a53258cf0fda576bc
Yashish Khurana <62921042+yashishkhurana@users.noreply.github.com>
2025-05-16 19:03:05 +0530

    feat: added jsonls to lsp.lua for formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,7 +4,7 @@ lsp.preset('recommended')
 
 require('mason').setup()
 require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'rust_analyzer' },
+    ensure_installed = { 'lua_ls', 'rust_analyzer', 'jsonls' },
     handlers = {
         lsp.default_setup,
     },
